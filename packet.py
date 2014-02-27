9543b8cf278a4caa9a4a5b83c5f6692b19822469
nbdt <dream@elk.noise.net>
2014-02-27 00:09:18 -0800

    clean up and error catching

--- packet.py
+++ packet.py
@@ -8,7 +8,6 @@ import time
 import serial
 import fftw3
 import numpy
-import signal
 
 class ThinkGearProtocol(object):
   syncnum = 2
@@ -24,7 +23,9 @@ class ThinkGearProtocol(object):
   disconnect_byte = 0xc1
   autoconnect_byte = 0xc2
   connected_code = 0xd0
+  headset_not_found = 0xd1
   disconnected_code = 0xd2
+  request_denied = 0xd3
   standby_code = 0xd4
   @staticmethod
   def checksum(seq):
@@ -36,15 +37,21 @@ class ThinkGearProtocol(object):
     elif codetype == ThinkGearProtocol.standby_code:
       return 1
     elif codetype == ThinkGearProtocol.connected_code:
+      return 3
+    elif codetype == ThinkGearProtocol.headset_not_found:
       return 2
     elif codetype == ThinkGearProtocol.disconnected_code:
-      return 2
+      return 3
+    elif codetype == ThinkGearProtocol.request_denied:
+      return 0
+    elif codetype == ThinkGearProtocol.standby_code:
+      return 1
     elif codetype == ThinkGearProtocol.eeg_data:
       return 2
     elif codetype == ThinkGearProtocol.power_bands:
       return 24
     else:
-      return 0
+      return -1
   @staticmethod
   def parse_eeg(data):
     a, b = data
@@ -66,63 +73,90 @@ class ThinkGearProtocol(object):
 class Device(object):
   protocol = ThinkGearProtocol
   def __init__(self):
-    self.baudrate = 57600 * 2
-    self.port = None
-    self.timeout = 1
-    self.ser = None
-    self.device_on()
-  def device_on(self):
-    for i in range(0,4):
-      self.port = '/dev/ttyUSB' + str(i)
-      try:
-        self.ser = serial.Serial(port=self.port, baudrate=self.baudrate, timeout=self.timeout)
-        # Test if this serial device is our device
-        od_avg = sum([ord(i) for i in self.ser.read(10)])/10
-        if od_avg > 50:
-          print od_avg
-          break
-        else:
-          continue
-      except:
-        continue
-    print self.ser
+    self.br = 57600 * 2
+    self.p = None
+    self.to = 1
+    self.ser = self.get_serial()
+    self.connecting = False
+  def get_serial(self, i=0):
+    self.p = '/dev/ttyUSB' + str(i)
+    ser = serial.Serial(port=self.p, baudrate=self.br, timeout=self.to)
+    if i == 5:
+      print "No serial connection made"
+      return False
+    if ser.isOpen() == False:
+      return self.get_serial(i+1)
+    return ser
   def control(self, val):
     self.ser.write(bytearray([val]))
   def connect(self):
+    if self.ser.isOpen() == False:
+      self.ser.open()
     self.control(self.protocol.autoconnect_byte)
+    self.connecting = True
   def disconnect(self):
     self.control(self.protocol.disconnect_byte)
+    if self.ser.isOpen():
+      self.ser.close()
+    self.connecting = False
   def bytevals(self):
-    while True:
-      yield self.ser.read()
+    while self.ser.isOpen() and self.connecting == True:
+      b = self.ser.read()
+      print hex(ord(b))
+      yield b
+
+class InvalidPacket(BaseException):
+  pass
+
+class InvalidPacketPaylen(InvalidPacket):
+  pass
+
+class InvalidPacketChecksum(InvalidPacket):
+  pass
+
+class InvalidPacketCodetype(InvalidPacket):
+  pass
+
+class InvalidPacketDatalen(InvalidPacket):
+  pass
 
 class Packet(object):
-  def __init__(self, src):
+  def __init__(self, coordinator):
+    self.coordinator = coordinator
     self.is_valid = False
-    self.paylen = src.next()
+    self.src = self.coordinator.bs.synced_src()
+    self.paylen = self.src.next()
     self.payload_data = None
     self.paycheck = None
-    if self.paylen <= ThinkGearProtocol.maxpay: 
-      self.payload_data = list(islice(src, self.paylen))
-      self.paycheck = src.next()
-      if self.paycheck == ThinkGearProtocol.checksum(self.payload_data):
-        self.is_valid = True
-  @property
+    if self.paylen > ThinkGearProtocol.maxpay: 
+      raise InvalidPacketPaylen
+    self.payload_data = list(islice(self.src, self.paylen))
+    self.paycheck = self.src.next()
+    if self.paycheck != ThinkGearProtocol.checksum(self.payload_data):
+      raise InvalidPacketChecksum
+    self.is_valid = True
+  def __iter__(self):
+    return self.payload_iterator()
   def payload_iterator(self):
+    if not self.is_valid:
+      raise InvalidPacket
     pos = 0
     while pos < self.paylen: 
       codetype = self.payload_data[pos]
       pos += 1
       expected_datalen = ThinkGearProtocol.datalen(codetype)
-      if expected_datalen < 1:
+      if expected_datalen == -1:
         self.is_valid = False
-        break
+        raise InvalidPacketDatalen
+      if expected_datalen == 0:
+        print codetype
+        continue
       if expected_datalen > 1:
         specified_datalen = self.payload_data[pos]
         pos += 1
         if specified_datalen != expected_datalen:
           self.is_valid = False
-          break
+          raise InvalidDatalen
       next_pos = pos + expected_datalen 
       dataslice = islice(self.payload_data, pos, next_pos)
       pos = next_pos
@@ -150,7 +184,6 @@ class Bytestream(object):
   @property
   def is_synced(self):
     return (self.syncer.count(ThinkGearProtocol.syncbyte) == ThinkGearProtocol.syncnum)
-  @property
   def synced_src(self):
     self.sync()
     return self.src
@@ -165,17 +198,39 @@ class Bytestream(object):
     self.sync_count += 1
 
 class Coordinator(object):
-  def __init__(self):
+  def __init__(self, MODE_UI, MODE_FFT):
+    self.ui_mode = MODE_UI
+    self.fft_mode = MODE_FFT
     self.dev = Device()
     self.bs = Bytestream(self.dev)
     self.connected = False
-    self.ui = UI(self)
-    self.handlers = self.init_handlers()
     self.logfile = open("logfile", "w")
     self.datafile = open("datafile", "w")
-    self.ui.app.exec_()
-    # never ... mind
-  def init_handlers(self):
+    self.handlers = self.make_handlers()
+    self.ui = self.make_ui()
+  def launch_ui(self):
+    self.ui.app.exec_()  
+  def make_ui(self):
+    if self.ui_mode == 'none':
+      self.ui = None
+    elif self.ui_mode == 'plots':
+      self.ui = UI_for_plots(self, self.fft_mode)
+      self.launch_ui()
+      # never ... mind
+    elif self.ui_mode == 'ep':
+      self.ui = UI_for_ep(self)
+  def make_handlers(self):
+    if self.ui_mode == 'none':
+      return self.init_headless_handlers()
+    elif self.ui_mode == 'plots':
+      return self.init_plot_handlers()
+    elif self.ui_mode == 'ep':
+      return self.init_ep_handlers()
+  def init_headless_handlers(self):
+    return dict()
+  def init_ep_handlers(self):
+    return dict()
+  def init_plot_handlers(self):
     d = {
       ThinkGearProtocol.signal_quality : self.signal_quality_handler(),
       ThinkGearProtocol.esense_attention : self.esense_attention_handler(),
@@ -207,14 +262,19 @@ class Coordinator(object):
     self.dev.connect()
     print "sent connect"
     attempts = 0
+    print "retry number: %i" % (5-retry)
     while not self.connected and attempts < 1024:
       self.dev.connect()
-      p = Packet(self.bs.synced_src)
-      if not p.is_valid:
+      attempts += 1
+      print "attempt number: %i" % attempts
+      print "are we connected? %r" % self.connected
+      try:
+        p = Packet(self)
+        print p
+      except InvalidPacket:
         print "invalid packet"
-        self.log(p)
         continue
-      for codetype, data in p.payload_iterator: 
+      for codetype, data in p:
         if codetype == ThinkGearProtocol.connected_code:
           print "connected"
           self.connected = True
@@ -222,7 +282,8 @@ class Coordinator(object):
         else:
           "got something other than connection code in packet"
           self.log(codetype, data)
-      attempts += 1
+      if not p.is_valid:
+        self.log(p)
     if not self.connected and (retry > 0):
       self.dev.disconnect()
       time.sleep(hangtime)
@@ -232,21 +293,19 @@ class Coordinator(object):
       print "exceeded max retrys, failure"
   def receive(self):
     while self.connected:
-      p = Packet(self.bs.synced_src)
+      p = Packet(self)
       if not p.is_valid:
         self.log(p)
-        continue
-      for codetype, data in p.payload_iterator: 
+      for codetype, data in p:
         handler = self.handlers.get(codetype)
         if handler is None:
-          self.log(codetype, data)
-          continue
+          self.log(p)
+          raise InvalidPacketCodetype 
         handler.send(data)
   def signal_quality_handler(self):
     while True:
       data = yield
       val, = data
-      self.ui.signalquality_plot_.send(val)
   def esense_attention_handler(self):
     while True:
       data = yield
@@ -277,63 +336,60 @@ class Coordinator(object):
   def cleanup(self):
     self.logfile.close()
     self.datafile.close()
+    self.dev.ser = serial.Serial(self.dev.port)
+    self.dev.ser.flush()
     self.dev.ser.close()
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-class UI(object):
-  def __init__(self, coordinator):
+class UI_for_plots(object):
+  def __init__(self, coordinator, MODE_FFT):
     self.coordinator = coordinator
     self.app = QtGui.QApplication(sys.argv)
+    # note might put this in global namespace ?
     self.widget = QtGui.QWidget()
     self.connect_btn = QtGui.QPushButton('Connect')
     self.disconnect_btn = QtGui.QPushButton('Disconnect')
     self.acquire_btn = QtGui.QPushButton('Acquire')
-    self.record_chkbx = QtGui.QCheckBox('Record Data')
     self.rawplot = pg.PlotWidget()
     self.ns_fftplot = pg.PlotWidget()
     self.rd_fftplot = pg.PlotWidget()
     self.rawplot.setRange(yRange=(500, -500))
-    self.ns_fftplot.setRange(yRange=(20, 0))
+    self.ns_fftplot.setRange(yRange=(0, 2))
     self.rd_fftplot.setRange(yRange=(0, 90))
     self.layout = QtGui.QGridLayout()
-    self.widget.setLayout(self.layout)
     self.layout.addWidget(self.connect_btn, 0, 0)
     self.layout.addWidget(self.disconnect_btn, 0, 1)
     self.layout.addWidget(self.acquire_btn, 0, 2)
-    self.layout.addWidget(self.record_chkbx, 0, 3)
     self.layout.addWidget(self.rawplot, 1, 0, 2, 4)
     self.layout.addWidget(self.ns_fftplot, 3, 0, 2, 4)
     self.layout.addWidget(self.rd_fftplot, 5, 0, 2, 4)
-    self.record_chkbx.stateChanged.connect(self.write_file)
+    self.widget.setLayout(self.layout)
     self.connect_btn.clicked.connect(self.send_connect)
     self.disconnect_btn.clicked.connect(self.send_disconnect)
     self.acquire_btn.clicked.connect(self.acquire)
     self.widget.show()
     self.raw_x = deque([0], 1024)
     self.raw_y = deque([0], 1024)
-    self.raw_plot_ = self.raw_plot()
+    self.raw_plot_ = self.raw_plot(fft_mode=MODE_FFT)
     self.raw_plot_.send(None)
     self.ns_fft_plot_ = self.ns_fft_plot()
     self.ns_fft_plot_.send(None)
-    self.sq_x = deque([0], 2)
-    self.sq_y = deque([0], 2)
-    self.signalquality_plot_ = self.signalquality_plot()
-    self.signalquality_plot_.send(None)
   def send_connect(self):
     self.coordinator.connect()
   def send_disconnect(self):
     print "sending disconnect"
     self.raw_x.clear()
     self.raw_y.clear()
-    self.sq_x.clear()
-    self.sq_y.clear()
     self.coordinator.disconnect()
     print "disconnected"
   def acquire(self):
-    self.coordinator.receive()
+    while True:
+        try:
+          self.coordinator.receive()
+        except InvalidPacket:
+          print "done receive?"
   def write_file(self):
     pass
-  def raw_plot(self):
+  def raw_plot(self, fft_mode):
     fft_size = 512
     bins = [i for i in range(fft_size/2)]
     while True:
@@ -347,34 +403,43 @@ class UI(object):
         #inputa = inputa * hann_window
         #inputa = inputa * flattop_window
         outputa = numpy.zeros(fft_size, dtype=complex)
-        fft = fftw3.Plan(inputa, outputa, direction='forward', flags=['estimate'])
-        fft.execute()
-        outputa = (numpy.log10(numpy.abs(outputa)) * 20)[:fft_size/2]
+        if fft_mode == '1':
+          fft = fftw3.Plan(inputa, outputa, direction='forward', flags=['estimate'])
+          fft.execute()
+          outputa = (numpy.log10(numpy.abs(outputa)) * 20)[:fft_size/2]
+          self.rd_fftplot.plot(bins, outputa, clear=True)
         self.rawplot.plot(self.raw_x, self.raw_y, clear=True)
-        self.rd_fftplot.plot(bins, outputa, clear=True)
         pg.QtGui.QApplication.processEvents()
   def ns_fft_plot(self):
     p_bands = range(8)
     while True:
       vals = yield
-      ln_vals = numpy.log(vals)
+      ln_vals = numpy.log(vals)/10
       self.ns_fftplot.plot(p_bands, ln_vals, clear=True, symbol='s', pen=None)
-  def signalquality_plot(self):
-    sq_num = 0
-    while True:
-      val = yield
-      self.sq_x.append(sq_num)
-      self.sq_y.append(val)
-      #self.sqplot.plot(self.sq_x, self.sq_y, clear=True)
-      sq_num += 1
       
+class UI_for_ep(object):
+  def __init__(self, coordinator):
+    pass
 
-def main():
+def main(ui=None, fft=None):
+  c = None
   try:
-    c = Coordinator()
+    c = Coordinator(MODE_UI=ui, MODE_FFT=fft)
   finally:
-    c.cleanup()
+    if c:
+      c.cleanup()
 
 if __name__ == '__main__':
-  main()
+  cli = sys.argv
+  for arg in cli:
+    if '--ui=' in arg:
+      ui = arg.split("=")[1]
+    elif '--fft=' in arg:
+      fft = arg.split("=")[1]
+    elif '--help' in arg:
+        print '--ui={none, plots}'
+        print '--fft={0, 1}'
+        sys.exit()
+  status_code = main(ui=ui, fft=fft)
+  sys.exit(status_code)
 
